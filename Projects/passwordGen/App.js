edd2db032e05196453760feb84c756dc05ceae48
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-07-27 23:25:24 +0545

    form validation

--- Projects/passwordGen/App.js
+++ Projects/passwordGen/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+//form validation
+import * as Yup from 'yup';
+
+const passwordSchema=Yup.object().shape({
+  passwordLength:Yup.number,
+  
+})
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
