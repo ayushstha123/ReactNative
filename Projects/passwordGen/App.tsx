c40d9c406fdbe6f5f1aceda824a8b579ce29bfae
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-07-30 17:45:10 +0545

    password Generator

--- Projects/passwordGen/App.tsx
+++ Projects/passwordGen/App.tsx
@@ -49,9 +49,6 @@ export default function App() {
   }
 
 
-
-
-
   const createPassword = (characters: string,
     passwordLength: number) => {
     let result = ''
@@ -70,70 +67,180 @@ export default function App() {
     setsymbols(false);
   }
   return (
-    <ScrollView keyboardShouldPersistTaps='handled'>
+    <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={styles.container}>
       <SafeAreaView>
-        <View>
-          <Text>Password Generator</Text>
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>Password Generator</Text>
           <Formik
-       initialValues={{passwordLength: '' }}
-       validationSchema={passwordSchema}
-       onSubmit={values =>{
-        generatePasswordString(+values.passwordLength) //TODO plus makes it convert into number typescript
-        console.log(values)
-       }}
-     >
-       {({
-         values,
-         errors,
-         touched,
-         isValid,
-         handleChange,
-         handleSubmit,
-         handleReset,
-         /* and other goodies */
-       }) => (
-         <>
-         <View>
-          <View>
-            <Text>Password Length</Text>
-            {touched.passwordLength && errors.passwordLength&&(
-              <Text>{errors.passwordLength}</Text>
+            initialValues={{ passwordLength: '' }}
+            validationSchema={passwordSchema}
+            onSubmit={(values) => {
+              generatePasswordString(+values.passwordLength); // Convert to number
+              console.log(values);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              isValid,
+              handleChange,
+              handleSubmit,
+              handleReset,
+            }) => (
+              <>
+                <View style={styles.inputContainer}>
+                  <View style={styles.inputGroup}>
+                    <Text style={styles.label}>Password Length</Text>
+                    {touched.passwordLength && errors.passwordLength && (
+                      <Text style={styles.error}>{errors.passwordLength}</Text>
+                    )}
+                    <TextInput
+                      value={values.passwordLength}
+                      onChangeText={handleChange('passwordLength')}
+                      placeholder="Password Length"
+                      keyboardType="numeric"
+                      style={styles.input}
+                    />
+                  </View>
+                </View>
+                <View style={styles.checkboxContainer}>
+                  <View style={styles.checkboxGroup}>
+                    <BouncyCheckbox
+                      isChecked={lowerCase}
+                      onPress={() => setlowerCase(!lowerCase)}
+                      fillColor='green'
+                    />
+                    <Text>Include Lowercase</Text>
+                  </View>
+                  <View style={styles.checkboxGroup}>
+                    <BouncyCheckbox
+                      isChecked={upperCase}
+                      onPress={() => setupperCase(!upperCase)}
+
+                    />
+                    <Text>Include UpperCase</Text>
+                  </View>
+                  <View style={styles.checkboxGroup}>
+
+                    <BouncyCheckbox
+                      isChecked={numbers}
+                      onPress={() => setnumbers(!numbers)}
+
+                    /><Text>Include Numbers</Text>
+                  </View>
+                  <View style={styles.checkboxGroup}>
+
+                    <BouncyCheckbox
+                      isChecked={symbols}
+                      onPress={() => setsymbols(!symbols)}
+
+                    /><Text>Include Symbols</Text>
+                  </View>
+                </View>
+                <View style={styles.formAction}>
+                  <TouchableOpacity onPress={() => handleSubmit()} style={styles.button}>
+                    <Text style={styles.buttonText}>Generate Password</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => { handleReset(); resetPasswordState(); }} style={styles.button}>
+                    <Text style={styles.buttonText}>Reset</Text>
+                  </TouchableOpacity>
+                </View>
+                {isPassGenerated && (
+                  <View style={styles.passwordContainer}>
+                    <Text disabled={!isValid} style={styles.passwordText}>Generated Password:</Text>
+                    <Text selectable={true} style={styles.password}>{password}</Text>
+                  </View>
+                )}
+              </>
             )}
-            <TextInput value={values.passwordLength}
-            onChangeText={handleChange('passwordLength')}
-            placeholder='Password Length'
-            keyboardType='numeric'
-            ></TextInput>
-          </View>
-         </View>
-         <View></View>
-         <View></View>
-         <View></View>
-         <View></View>
-
-         <View style={styles.formAction}>
-          <TouchableOpacity>
-            
-            <Text>Generate Password</Text></TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Reset</Text>
-            </TouchableOpacity>
-         </View>
-
-         </>
-       )}
-     </Formik>
+          </Formik>
         </View>
       </SafeAreaView>
-    </ScrollView> 
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
+    backgroundColor: '#f5f5f5',
+    padding: 20,
+  },
+  innerContainer: {
     backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  inputGroup: {
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 10,
+    fontSize: 16,
+  },
+  error: {
+    fontSize: 14,
+    color: 'red',
+    marginTop: 5,
+  },
+  checkboxContainer: {
+    marginBottom: 20,
+
+  },
+  checkboxGroup: {
+    flexDirection: 'row',
+    marginRight: 20,
+    marginBottom: 10,
+
+  },
+  formAction: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    padding: 12,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  passwordContainer: {
+    marginTop: 20,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  passwordText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  password: {
+    fontSize: 24,
+    color: '#007bff',
+    marginTop: 10,
   },
 });
