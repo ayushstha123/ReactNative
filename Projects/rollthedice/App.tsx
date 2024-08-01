b5dba55109f8d2f5b3de5690dcab29cd1b8b3761
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-08-01 20:49:27 +0545

    rolling dice

--- Projects/rollthedice/App.tsx
+++ Projects/rollthedice/App.tsx
@@ -1,27 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import {  Image  , ImageSourcePropType, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 import DiceOne from './assets/One.png'
 import DiceTwo from './assets/Two.png'
 import DiceThree from './assets/Three.png'
-import DiceFour  from './assets/Four.png'
+import DiceFour from './assets/Four.png'
 import DiceFive from './assets/Five.png'
 import DiceSix from './assets/Six.png'
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 
-type DiceProps=PropsWithChildren<{
-  imageUrl:ImageSourcePropType
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
 }> //error prone approach for image url >best way to do it
-const Dice=({imageUrl}:DiceProps):JSX.Element =>{
-  return(
-    <Image style={styles.diceImage} source={imageUrl}/>
-  )
+/**
+ * This is a functional component that renders an Image component with a 
+ * specific image source. The image source is passed as a prop to the 
+ * component and is used to display a dice image.
+ *
+ * @param {DiceProps} props - An object containing the image source for the dice.
+ * @returns {JSX.Element} - A JSX element representing the Image component.
+ */
+const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
+  // Render an Image component with the specified image source
+  return (
+    <Image
+      // Apply the style defined in the stylesheet to the Image component
+      style={styles.diceImage}
+      // Specify the source of the image to be displayed
+      source={imageUrl}
+    />
+  );
 }
 
 
-export default function App():JSX.Element {
+export default function App(): JSX.Element {
+  //creating a state
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne)//useState<ImageSourcePropType>(DiceOne): The useState hook initializes the state. The generic type <ImageSourcePropType> specifies that the state will be of this type. DiceOne is the initial state value, which is a valid ImageSourcePropType.
+
+  const rollDice = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1; //here we are multipling by 6 and adding 1 to it to generate a random number between thats not zero
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne)
+        break;
+      case 2:
+        setDiceImage(DiceTwo)
+        break;
+      case 3:
+        setDiceImage(DiceThree)
+        break;
+      case 4:
+        setDiceImage(DiceFour)
+        break;
+      case 5:
+        setDiceImage(DiceFive)
+        break;
+      case 6:
+        setDiceImage(DiceSix)
+        break;
+      default:
+        setDiceImage(DiceOne)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Dice imageUrl={DiceOne}/>
+      <Dice imageUrl={diceImage} />
+      <Pressable>
+        <Text  style={styles.rollbutton} onPress={rollDice}>Roll The Dice</Text>
+      </Pressable>
     </View>
   );
 }
@@ -33,7 +78,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  diceImage:{
-    width:200,
+  diceImage: {
+    height: 200,
+    width: 200
+  },
+  rollbutton:{
+    marginTop: 20,
+    backgroundColor:'black',
+    color:'white',
+    padding:12
   }
 });
