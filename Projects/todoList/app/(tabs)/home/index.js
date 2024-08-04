a361b681341f97855ab0a1f8482be851e62f3100
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-08-04 20:14:57 +0545

    category style

--- Projects/todoList/app/(tabs)/home/index.js
+++ Projects/todoList/app/(tabs)/home/index.js
@@ -1,36 +1,38 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import AntDesign from '@expo/vector-icons/AntDesign';
 export default function index() {
   return (
-    <>
-      <View style={{marginHorizontal: 10,marginVertical:10, flexDirection: "row"}} >
+    <View style={{backgroundColor:"black",height:"100%"}}>
+      <View style={{gap:5,marginHorizontal: 10,marginVertical:10, flexDirection: "row"}} >
         <Pressable
-          style={{
-            margin:2,
-            backgroundColor: "black",
+          style={{   
+            backgroundColor: "white",
             paddingHorizontal: 10,
             paddingVertical: 6,
-            borderRadius: 25,
+            borderRadius: 50,
             alignItems: "center",
             justifyContent: "center",
           }}>
-          <Text style={{ color: "white", textAlign: "center" }}>All</Text>
+          <Text style={{ color: "black",justifyContent:"center", textAlign: "center" }}>All</Text>
         </Pressable>
         <Pressable
           style={{
-            margin:2,
-            backgroundColor: "black",
+
+            backgroundColor: "white",
             paddingHorizontal: 10,
             paddingVertical: 6,
-            borderRadius: 25,
+            borderRadius: 50,
             alignItems: "center",
             justifyContent: "center",
+            marginRight:"auto"
           }}>
-          <Text style={{ color: "white", textAlign: "center" }}>Personal</Text>
+          <Text style={{ color: "black", textAlign: "center" }}>Personal</Text>
         </Pressable>
+        <Pressable>
+        <AntDesign name="pluscircle" size={30} color="white" />        </Pressable>
       </View>
-    </>
+    </View>
   )
 }
 
