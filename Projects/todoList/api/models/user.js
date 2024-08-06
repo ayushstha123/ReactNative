fa79bd4e006c0dd027ae4a9ac9dd7d5a3b6354b8
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-08-06 14:57:11 +0545

    models: Todo and User

---
+++ Projects/todoList/api/models/user.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    todos:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Todo',
+    }
+},{ timestamps: true }
+)
+
+const User=mongoose.model("User".userSchema)
+module.exports=User
