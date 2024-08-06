644b216013979896a0c649d0d4360caa767d41cd
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-08-06 13:54:49 +0545

    login page

--- Projects/todoList/app/(authenticate)/login.js
+++ Projects/todoList/app/(authenticate)/login.js
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { KeyboardAvoidingView, TextInput } from 'react-native-web'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>login</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "black", alignItems: "center" }}>
+      <View style={{ marginTop: 100 }}>
+        <Text style={{
+          fontSize: 25,
+          fontWeight: "bold",
+          color: "white",
+          fontFamily: 'Arial',
+        }}>Login</Text>
+      </View>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: 'center' }}>
+          <Text style={{ color: "white", marginBottom: 10, marginTop: 10 }}>Log in to your account!</Text>
+        </View>
+        <View>
+          <View style={{flexDirection: "row", alignItems: "center",backgroundColor:"#E0E0E0",marginTop:30,paddingVertical:5,borderRadius:5 }}>
+            <MaterialIcons style={{color:"gray", margin: 10 }} name="email" size={28} color="black" />
+            <TextInput 
+            value={email}
+            onChangeText={(text)=>setEmail(text)}
+            placeholder='Email' style={{color:"gray", padding: 5,fontSize:17, margin: 5, width: 250, }} />
+          </View>
+          <View style={{flexDirection: "row", alignItems: "center",backgroundColor:"#E0E0E0",marginTop:30,paddingVertical:5,borderRadius:5 }}>
+          <AntDesign style={{color:"gray", margin: 10 }} name="eye" size={28} color="black" />
+            <TextInput 
+            value={password}
+            onChangeText={(text)=>setPassword(text)}
+            placeholder='Password' style={{color:"gray", padding: 5,fontSize:17, margin: 5, width: 250, }} />
+          </View>
+          <View style={{flexDirection:"row",alignItems:'center',justifyContent:'space-between',marginTop:12}}>
+            <Text style={{color:"white"}}>Keep me logged in</Text>
+            <Text style={{color:"red",fontWeight:500}}>Forgot password ?</Text>
+          </View>
+          <View style={{marginTop:60}}>
+            <Pressable style={{backgroundColor:"white",padding:10,alignItems:"center",justifyContent:"center",borderRadius:5}}>
+              <Text style={{color:"black",textAlign:'center',fontWeight:'bold',fontSize:16,fontFamily:'arial'}}>Login</Text>
+            </Pressable>
+            <Pressable style={{marginTop:16}}>
+              <Text style={{color:"gray",textAlign:'center',fontSize:15,fontFamily:'arial'}}>Dont have an account? Sign up</Text>
+            </Pressable>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
 export default login
 
-const styles = StyleSheet.create({})
--- Projects/todoList/app/index.js
+++ Projects/todoList/app/index.js
@@ -3,30 +3,6 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Page() {
   return (
-    <Redirect href="/(tabs)/home" />
+    <Redirect href="/(authenticate)/login" />
   );
 }
-//mongodb+srv://test:test@cluster0.yeol1vi.mongodb.net/
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-    backgroundColor:"black",
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
